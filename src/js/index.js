94bcccb3522d1ccdb54184ac271381b258e70131
Zuzanna Dąbrowska <suzie.dabrowska@gmail.com>
2020-07-28 14:33:13 +0200

    clear button

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,22 @@
 import '../scss/main.scss';
 
-/* place your code below */
+
 
 const textarea = document.querySelector('.textarea--js');
 const save = document.querySelector('.save--js');
 const load = document.querySelector('.load--js');
-
+const clear = document.querySelector('.clear--js');
 
 save.addEventListener('click', (e) => {
-    e.preventDefault();
-  localStorage.setItem('entry', textarea.value);  
+  e.preventDefault();
+  localStorage.setItem('entry', textarea.value);
 })
 
 load.addEventListener('click', (e) => {
-    e.preventDefault();
-    textarea.value = localStorage.getItem('entry');
+  e.preventDefault();
+  textarea.value = localStorage.getItem('entry');
+});
+
+clear.addEventListener('click', () => {
+  localStorage.removeItem('entry')
 })
