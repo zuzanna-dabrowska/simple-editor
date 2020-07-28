018ca2b3d3eb8c1c5cef85525cfe944c88e08b42
Zuzanna Dąbrowska <suzie.dabrowska@gmail.com>
2020-07-28 12:26:13 +0200

    js

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,12 @@ const save = document.querySelector('.save--js');
 const load = document.querySelector('.load--js');
 
 
-save.addEventListener('click', () => {
+save.addEventListener('click', (e) => {
+    e.preventDefault();
   localStorage.setItem('entry', textarea.value);  
+})
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    textarea.value = localStorage.getItem('entry');
 })
